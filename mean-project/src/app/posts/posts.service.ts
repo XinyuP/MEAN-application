136173fe0e41e8f01c528dbbf3bd151f10ffdc39
XinyuP <xinyup@umich.edu>
2023-02-14 01:01:28 -0500

    fetch post from Angular Frontend using HttpClient

--- mean-project/src/app/posts/posts.service.ts
+++ mean-project/src/app/posts/posts.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Post } from './post.model';
 import { Subject } from 'rxjs';
 
@@ -7,8 +8,33 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts]; // make copy of posts array
+    this.http
+      .get<{ message: string; posts: Post[] }>(
+        'http://localhost:3000/api/posts'
+      )
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        // no need to use copy --> it comes from the server, we won't accidently change it on the server,
+        // there is no such connection, it was part of http response
+
+        this.postsUpdated.next([...this.posts]);
+        // inform about this update
+      });
+
+
+
+
+    // I wanna reach out to backend, fetch the posts, store them in posts variable here
+    // and then fire my update listener to inform everyone interested in my app
+
+    // we want to send http request here --> Angular has built-in http client
+
+    // you can inject things into services too, not just in components
+
+    // return [...this.posts]; // make copy of posts array
     // use [] to create new array
     // ... take all the element of posts array
     // pul them out of that array and add them to the new array
@@ -16,10 +42,11 @@ export class PostsService {
 
   getPostsUpdatedListener() {
     return this.postsUpdated.asObservable();
-  }  // it returns an object which we can listen but we cannot emit
+  } // it returns an object which we can listen but we cannot emit
 
   addPost(title: string, content: string) {
     const post: Post = {
+      id: null,
       title: title,
       content: content,
     };
