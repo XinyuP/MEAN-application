6410b81a4253dad421d5c389bb079a6bd3d3e3ad
XinyuP <xinyup@umich.edu>
2023-02-13 19:36:53 -0500

    add angular material

--- mean-project/src/app/app.module.ts
+++ mean-project/src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    PostCreateComponent
-  ],
+  declarations: [AppComponent, PostCreateComponent],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
-
+export class AppModule {}
 
 // this file defines the features our Angular App has
 // Applications are split up in modules
--- mean-project/src/app/posts/post-create/post-create.component.ts
+++ mean-project/src/app/posts/post-create/post-create.component.ts
@@ -5,9 +5,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css'],
 })
 export class PostCreateComponent {
-  enteredValue =''
+  enteredValue = '';
   newPost = 'No';
 
   onAddPost() {
