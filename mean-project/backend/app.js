43dfba8359c649cd7739aba990a59e9c6dd32fef
XinyuP <xinyup@umich.edu>
2023-02-14 00:40:07 -0500

    modify server.js, add /api/posts

--- mean-project/backend/app.js
+++ mean-project/backend/app.js
@@ -4,10 +4,43 @@ const express = require("express");
 
 const app = express();
 
+
+app.use("/api/posts", (req, res, next) => {
+  // adding api is optional, just to make it clear
+  const posts = [
+    {
+      id: "213221321",
+      title: "wedewdwe",
+      content: "dwdewdwds",
+    },
+    {
+      id: "23231",
+      title: "wedewdwe",
+      content: "dwdewdwds",
+    },
+    {
+      id: "323213",
+      title: "wedewdwe",
+      content: "dwdewdwds",
+    },
+    {
+      id: "21321",
+      title: "wedewdwe",
+      content: "dwdewdwds",
+    },
+  ];
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts,
+  });
+  next();
+});
+
 // express app is just a big chain of middlewares we apply to the incoming requests
 // like a funnel through which we send that express and every part of that funnel or in
 // that funnel, we have different parts, and every part can do something with request.
 
+/*
 app.use((req, res, next) => {
   console.log("heljdsjdsjdkjd");
   next();
@@ -16,5 +49,5 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   res.send("hello for express");
 }); //use a new middleware on our app and incoming request
-
+*/
 module.exports = app;
--- mean-project/server.js
+++ mean-project/server.js
@@ -1,3 +1,60 @@
+const app = require("./backend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
+const server = http.createServer(app);
+
+const normalizePort = val => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+server.on("error", onError);
+server.on("listening", onListening);
+server.listen(port);
+
+
+
+
+/*
 const http = require("http"); // default node.js package
 // require is node.js import syntax, import the package, and store its content in the variable
 const app = require("./backend/app");
@@ -12,3 +69,4 @@ app.set("port", port); // set config for my express environment
 const server = http.createServer(app);
 
 server.listen(port);
+*/
